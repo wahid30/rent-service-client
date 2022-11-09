df8ed2ebaea473c146bfdd68e24ea1474e29b58a
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-09 23:47:36 +0600

    review added

---
+++ src/Pages/AllServices/Reviews/Reviews.jsx
@@ -0,0 +1,88 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider";
+
+const Reviews = ({ service }) => {
+  const { _id, name, img, details, rating, price } = service;
+  const { user } = useContext(AuthContext);
+  // console.log(service);
+  const handlePlaceOrder = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = form.name.value;
+    const photo = form.photo.value;
+    const userRating = form.rating.value;
+    const email = form?.email.value || "unregistered";
+    const message = form.message.value;
+
+    const review = {
+      service: _id,
+      serviceName: name,
+      price,
+      photo,
+      customer: userName,
+      email,
+      rating: userRating,
+      message,
+    };
+    // console.log(review);
+
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          alert("review Placed Successfully");
+          form.reset();
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="my-5">
+      <form onSubmit={handlePlaceOrder}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          <input
+            name="name"
+            type="text"
+            placeholder="Your Name"
+            className="input input-bordered w-full"
+          />
+          <input
+            name="photo"
+            type="url"
+            placeholder="Your photo url"
+            className="input input-bordered w-full"
+          />
+          <input
+            name="rating"
+            type="text"
+            placeholder="Rating"
+            className="input input-bordered w-full"
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="Your Email"
+            defaultValue={user?.email}
+            className="input input-bordered w-full"
+            readOnly
+          />
+        </div>
+        <textarea
+          name="message"
+          className="mt-3 textarea w-full textarea-bordered h-[140px]"
+          placeholder="Your Message"
+        ></textarea>
+        <input className="btn my-3" type="submit" value="Add Your Review" />
+      </form>
+    </div>
+  );
+};
+
+export default Reviews;
--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import useTitle from "../../hooks/useTitle";
+import Reviews from "../AllServices/Reviews/Reviews";
 
 const Details = () => {
+  useTitle("Details");
   const service = useLoaderData();
-  console.log(service);
-  return <div>this is details</div>;
+  const { _id, name, img, details, rating, price } = useLoaderData();
+
+  return (
+    <div className="my-5">
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">{name}</h1>
+            <p className="mb-5 text-xl">{details}</p>
+            <p className="text-2xl">Rating: {rating}</p>
+            <p className="text-2xl">Price: {price}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Reviews service={service}></Reviews>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
